902b2ac1f06df9a45c1edab9a4c9aed72f0dc0b4
devangshrivastava <devangshrivastava27@gmail.com>
2024-04-28 17:25:18 +0530

    gen_task and check added all working good

--- client.cc
+++ client.cc
@@ -14,14 +14,25 @@ vector<int> random_server_list(int num_server) {
     return server_list;
 }
 
-vector<vector<int>> gen_task(int num_server){
-    vector<vector<int>> result;
-    for (int i = 0; i < num_server; i++){
-        result.push_back(std::vector<int>());
-        for (int j = 0; j < 5; j++)
-            result[i].push_back(std::rand()%100);
-    }
-    return result;
+bool check(vector<vector<int>> task, int task_id){
+   int n = task.size();
+   for (int i = 0; i < n-1; i++){
+       if(task[i][5] == task_id) return false;
+   }
+   return true;
+}
+
+vector<vector<int>> gen_task(int x){
+   vector<vector<int>> result;
+   for (int i = 0; i < x; i++){
+       result.push_back(std::vector<int>());
+       for (int j = 0; j < 5; j++)
+           result[i].push_back(std::rand()%100);
+       int task_id = rand()%x;
+       while(check(result, task_id)) task_id = rand()%x;
+       result[i].push_back(task_id);  // last element as ID
+   }
+   return result;
 }
 
 
@@ -35,13 +46,8 @@ void Client::initialize(){
     vector<vector<int>> arr_1 = gen_task(num_server);
 
     int moduleId = getId(); // Getting the module's own
-//    for (int i = 0; i < num_server; i++){
-//        for (int j = 0; j < 5; j++) EV<<arr_1[i][j]<<" ";
-//        EV<<"\n";
-//
-//    }
-//  EV << "Client " << moduleId - 2 << " send a message \n";
-//    send_message({1},1,2,1);
+
+
     EV << "My module ID is: " << moduleId << endl;
     for (int i = 0; i < gateSize("out"); i++) {
             EV << "Gate out[" << i << "] is connected to " << gate("out", i)->getPathEndGate()->getOwnerModule()->getFullPath() << endl;
@@ -53,58 +59,7 @@ void Client::initialize(){
 
 }
 
-// return total and score of server as well
-int calculate_score(vector<vector<int>> &task_result, vector<int> &scores,int num_server){
-    int s = 0;
-    for(int i = 0; i < task_result.size(); i++){
-        map<int,int> freq;
-        int max_freq = -1;
-        int max_freq_elem = -1;
 
-        for(int j = 0; j < task_result[i].size(); j++){
-            if(task_result[i][j] != -1){
-                freq[task_result[i][j]]++;
-            }
-        }
-
-        for(auto it = freq.begin(); it != freq.end(); it++){
-            if(it->second >= max_freq){
-                max_freq_elem = it->first;
-                max_freq = it->second;
-            }
-        }
-        for(int j = 0; j < task_result[i].size(); j++){
-            if(task_result[i][j] == max_freq_elem && task_result[i][j] != -1){
-                scores[j]++;
-            }
-        }
-        s += max_freq_elem;
-    }
-    return s;
-}
-
-string vectorToString(const std::vector<int>& vec) {
-    stringstream ss;
-    for (int i : vec) {
-        ss << i;
-    }
-    return ss.str();
-}
-
-vector<int> firstKHighestIndices(const vector<int>& vec, int k) {
-    vector<int> indices(vec.size());
-    for (int i = 0; i < indices.size(); ++i) {
-        indices[i] = i;
-    }
-
-
-    partial_sort(indices.begin(), indices.begin() + k, indices.end(),[&vec](int a, int b) { return vec[a] > vec[b]; });
-
-    return std::vector<int>(indices.begin(), indices.begin() + k);
-}
-//
-
-//}
 
 void Client::handleMessage(cMessage *msg){
        if(t == 2) finish();
